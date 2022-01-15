f21499d61ae5dfee20d04a559c9b67aebd90983e
Leyan Lo <leyanlo@gmail.com>
2022-01-14 21:54:26 -0800

    feat: handle duplicate letters, update algorithm to minimize variance
    - Handle duplicate letters where one is marked present and the other absent. Before we would fail to find the solution because we didn’t check against allPresent.
    - Update algorithm to minimize variance, which starts us off with slate instead of raise.
    - Hard-code array length to 3 ** 5 for minor optimization.

--- src/content-script.js
+++ src/content-script.js
@@ -7,13 +7,9 @@ const board = [
   }))
 );
 
-const CORRECT = 0;
-const PRESENT = 1;
-const ABSENT = 2;
-
 const correct = Array(5).fill(null);
 const present = [...Array(5)].map(() => []);
-const absent = [];
+const absent = [...Array(5)].map(() => []);
 
 outer: for (const row of board) {
   for (let i = 0; i < row.length; i++) {
@@ -26,7 +22,7 @@ outer: for (const row of board) {
         present[i].push(letter);
         break;
       case 'absent':
-        absent.push(letter);
+        absent[i].push(letter);
         break;
       default:
         // end of rows
@@ -34,6 +30,9 @@ outer: for (const row of board) {
     }
   }
 }
+
+const allPresent = present.flat();
+const allAbsent = absent.flat();
 
 fetch(chrome.runtime.getURL('assets/wordlist.json'))
   .then((response) => response.json())
@@ -45,42 +44,35 @@ fetch(chrome.runtime.getURL('assets/wordlist.json'))
           .every((char, i) =>
             correct[i]
               ? correct[i] === char
-              : !present[i].includes(char) && !absent.includes(char)
-          ) && present.flat().every((char) => word.includes(char))
+              : present[i].includes(char) || absent[i].includes(char)
+              ? false
+              : allAbsent.includes(char)
+              ? allPresent.includes(char)
+              : true
+          ) && allPresent.every((char) => word.includes(char))
     );
 
     let bestWord = null;
 
     // avoid computation if first word
     if (words.length === 2315) {
-      bestWord = 'raise';
+      bestWord = 'slate';
     } else {
-      let minEv = 1;
+      let minVariance = words.length ** 2;
       for (const a of words) {
-        const counts = {};
+        const counts = Array(3 ** 5).fill(0);
         for (const b of words) {
           const evals = [];
           for (let i = 0; i < b.length; i++) {
-            evals.push(
-              b[i] === a[i] ? CORRECT : a.includes(b[i]) ? PRESENT : ABSENT
-            );
-            const key = evals.join('');
-            counts[key] = (counts[key] ?? 0) + 1;
+            evals.push(b[i] === a[i] ? 0 : a.includes(b[i]) ? 1 : 2);
           }
+          counts[parseInt(evals.join(''), 3)]++;
         }
-
-        let ev = 0;
-        for (let i = 0; i < 3 ** 5; i++) {
-          const key = i.toString(3).padStart(5, '0');
-          let product = 1;
-          for (let j = 0; j < 5; j++) {
-            product *= (counts[key.slice(0, j + 1)] ?? 0) / words.length;
-          }
-          ev += product;
-        }
-
-        if (ev <= minEv) {
-          minEv = ev;
+        const variance = counts
+          .map((count) => count ** 2)
+          .reduce((acc, n) => acc + n);
+        if (variance < minVariance) {
+          minVariance = variance;
           bestWord = a;
         }
       }
@@ -88,10 +80,12 @@ fetch(chrome.runtime.getURL('assets/wordlist.json'))
 
     return Promise.resolve({ words, bestWord });
   })
-  .then(({ words, bestWord }) =>
-    console.log(
-      words.length > 1
-        ? `${words.length} words are possible. Best guess: ${bestWord}`
-        : `The answer is ${bestWord}!`
-    )
-  );
+  .then(({ words, bestWord }) => {
+    if (words.length === 1) {
+      console.log(`1 word is possible. Best guess: ${bestWord}`);
+    } else if (words.length > 1) {
+      console.log(
+        `${words.length} words are possible. Best guess: ${bestWord}`
+      );
+    }
+  });
