15f9a6f39ae7912f94cae42a6fafccfea19f77b1
Leyan Lo <leyanlo@gmail.com>
2022-01-29 00:23:03 -0800

    feat(script): calculate distributions for the 10 best words

--- scripts/distribution.js
+++ scripts/distribution.js
@@ -8,41 +8,42 @@ const allWords = JSON.parse(
 
 const allEvs = JSON.parse(fs.readFileSync('../src/assets/evs.json', 'utf8'));
 
-const nGuessesMap = {};
-outer: for (const answer of allWords) {
-  const board = [];
-  let guess = 'raise';
-  let nGuesses = 1;
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    let tiles = getTiles(guess, answer);
-    if (tiles.every((t) => t === 'correct')) {
-      nGuessesMap[answer] = nGuesses;
-      continue outer;
-    }
-    let row = [];
-    for (let i = 0; i < 5; i++) {
-      row.push({ letter: guess[i], evaluation: tiles[i] });
-    }
-    board.push(row);
+// calculate distributions for the 10 best words
+for (const firstGuess of Object.keys(allEvs).slice(0, 10)) {
+  console.log(`First guess: ${firstGuess}`);
+  const nGuessesMap = {};
+  outer: for (const answer of allWords) {
+    const board = [];
+    let guess = firstGuess;
+    let nGuesses = 1;
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      let tiles = getTiles(guess, answer);
+      if (tiles.every((t) => t === 'correct')) {
+        nGuessesMap[answer] = nGuesses;
+        continue outer;
+      }
+      let row = [];
+      for (let i = 0; i < 5; i++) {
+        row.push({ letter: guess[i], evaluation: tiles[i] });
+      }
+      board.push(row);
 
-    const { bestWords } = processBoard(board, allWords, allEvs);
-    guess = bestWords[0];
-    nGuesses++;
+      const { bestWords } = processBoard(board, allWords, allEvs);
+      guess = bestWords[0];
+      nGuesses++;
+    }
   }
-}
 
-console.log('nGuessesMap:', nGuessesMap);
-
-const nGuesses = Object.values(nGuessesMap);
-console.log('Average:', nGuesses.reduce((acc, n) => acc + n) / nGuesses.length);
-
-const distribution = {};
-for (const n of nGuesses) {
-  distribution[n] = (distribution[n] ?? 0) + 1;
-}
-console.log(`Distribution:
-# guesses  # words
+  const nGuesses = Object.values(nGuessesMap);
+  const distribution = {};
+  for (const n of nGuesses) {
+    distribution[n] = (distribution[n] ?? 0) + 1;
+  }
+  console.log(`# guesses  # words
 ${Object.entries(distribution)
-  .map((entry) => entry.map((n) => n.toString().padStart(7, ' ')).join('   '))
-  .join('\n')}`);
+  .map((e) => e.map((n) => n.toString().padStart(9, ' ')).join(''))
+  .join('\n')}
+Average: ${nGuesses.reduce((acc, n) => acc + n) / nGuesses.length}
+`);
+}
